e441170aedd7977ed051c5dbd4af879f31db7f27
Tsegazeab <tsegazeabwuta@gmail.com>
2024-03-09 18:06:13 -0800

    mongoDB database connection is implemented

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const app = express()
 
+// connect the mongodb atlas database using url
+
+mongoose.connect(
+  process.env.MONGODB
+).then(()=>console.log("MongoDB is connected"))
+.catch((err)=>console.log(err))
+
+
 app.listen(3000, ()=>console.log("server listening to port 3000"))
+
+// kdSGmaPJexBQO6ch
+// tm-dev
